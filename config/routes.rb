154a0cac073335fc3187c0b16bb263a23746890b
ad <chamalo.prv@gmail.com>
2024-02-23 17:28:48 +0100

    dazdz

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :karts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
-  root "karts#index"
+  root 'karts#index'
+  resources :karts
 end
