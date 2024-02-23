139df973df30b0f48895f99b9550683e9fe60a86
Barth0 <barthoduteil@gmail.com>
2024-02-23 15:53:01 +0100

    oubli point virgule

--- app/assets/javascripts/script.js
+++
@@ -1,8 +0,0 @@
-// app/assets/javascripts/script.js
-
-document.addEventListener('DOMContentLoaded', function() {
-    var imageFooter = document.getElementById('imageFooter'); 
-    imageFooter.addEventListener('click', function() {
-      document.body.classList.toggle('gradient-background');
-    });
-  });
